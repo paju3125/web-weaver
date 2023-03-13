f70799a7d17cda878b23481cc5a81896dec405eb
paju3125 <prajvalgandhi483@gmail.com>
2023-03-13 20:41:07 +0530

    PHP apis added to submit forms

--- src/components/home/Booking.js
+++ src/components/home/Booking.js
@@ -1,54 +1,112 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function Booking() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [mob, setMob] = useState('')
+    const [roomtype, setRoomType] = useState('')
+    const [checkIn, setCheckIn] = useState('')
+    const [checkOut, setCheckOut] = useState('')
+    const [req, setReq] = useState('')
+    const [guest, setGuest] = useState('')
+
+    const handleSubmit = () => {
+        console.log(name, email, mob, roomtype, checkIn, checkOut, req, guest)
+        if (name.length === 0) {
+            alert("Name has left blank");
+        }
+        else if (mob.length === 0) {
+            alert("Mobile number has left blank");
+        }
+        else if (mob.length !== 10) {
+            alert("Mobile number can be 10 digits long");
+        }
+        else if (email.length === 0) {
+            alert("Email id has left blank");
+        }
+        else if (roomtype.length === 0) {
+            alert("Room type has left blank");
+        }
+        else if (checkIn.length === 0) {
+            alert("CheckIn has left blank");
+        }
+        else if (checkOut.length === 0) {
+            alert("CheckOut has left blank");
+        }
+        else if (req.length === 0) {
+            alert("Special request has left blank");
+        }
+        else if (guest.length === 0) {
+            alert("Guest has left blank");
+        }
+        else {
+            const url = 'http://localhost/webweaver/room_book.php';
+            let fData = new FormData();
+            fData.append('name', name);
+            fData.append('email', email);
+            fData.append('mob', mob);
+            fData.append('roomtype', roomtype);
+            fData.append('checkIn', checkIn);
+            fData.append('checkOut', checkOut);
+            fData.append('req', req);
+            fData.append('guest', guest);
+
+            axios.post(url, fData)
+                .then(response => alert(response.data))
+                .catch(error => alert(error))
+        }
+    }
+
     return (
         <section id="booking">
             <h2 className="sec-head mt-2">Book Your stay here</h2>
             <div className="search-box container" >
-                <form className="input-box" method="post" action="book_room.php" >
+                {/* method="post" action="book_room.php"  */}
+                <form className="input-box">
                     <div className="input-box mb-3">
                         <label className="form-label" htmlFor="b_name">Name</label>
-                        <input className="form-control" type="text" name="" id="b_name" placeholder="Your good name" />
+                        <input className="form-control" type="text" name="" id="b_name" value={name} placeholder="Your good name" onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="input-box mb-3">
                         <label className="form-label" htmlFor="b_mob">Mobile Number</label>
-                        <input className="form-control" type="number" name="" id="b_mob" placeholder="Your Mobile number" />
+                        <input className="form-control" type="number" name="" id="b_mob" value={mob} placeholder="Your Mobile number" onChange={(e) => setMob(e.target.value)} />
                     </div>
                     <div className="input-box mb-3">
                         <label className="form-label" htmlFor="b_email">Email-id</label>
-                        <input className="form-control" type="email" name="" id="b_email" placeholder="your mail id" />
+                        <input className="form-control" type="email" name="" id="b_email" value={email} placeholder="your mail id" onChange={(e) => setEmail(e.target.value)} />
                     </div>
 
                     <div className="input-box mb-3">
 
                         <label className="form-label" htmlFor="cars">Room Type</label>
-                        <select id="cars" className='form-select' >
-                            <option value="Deluxroom">Select your room </option>
+                        <select id="cars" className='form-select' value={roomtype} onChange={(e) => setRoomType(e.target.value)} >
+                            <option value="">Select your room </option>
                             <option value="Deluxroom">Delux room </option>
                             <option value="Doubledelux">Double delux</option>
                             <option value="Executivedelux">Executive bedroom </option>
-
                         </select>
                     </div>
                     <div className="input-box mb-3">
                         <label className="form-label" htmlFor="b_checkIn">Check-In Date</label>
-                        <input className="form-control" type="date" name="" id="b_checkIn" placeholder="01/01/2021" />
+                        <input className="form-control" type="date" name="" id="b_checkIn" value={checkIn} placeholder="01/01/2021" onChange={(e) => setCheckIn(e.target.value)} />
                     </div>
                     <div className="input-box mb-3">
                         <label className="form-label" htmlFor="b_checkOut">Check-out Date</label>
-                        <input className="form-control" type="date" name="" id="b_checkOut" placeholder="01/01/2021" />
+                        <input className="form-control" type="date" name="" value={checkOut} id="b_checkOut" placeholder="01/01/2021" onChange={(e) => setCheckOut(e.target.value)} />
                     </div>
                     <div className="input-box mb-3">
                         <label className="form-label" htmlFor="special_requests">Special requests:</label>
-                        <textarea className='form-control' id="special_requests" name="special_requests"></textarea>
+                        <textarea className='form-control' id="special_requests" value={req} name="special_requests" onChange={(e) => setReq(e.target.value)} ></textarea>
                     </div>
                     <div className="input-box mb-5">
                         <label className="form-label" htmlFor="b_guest">Guests</label>
-                        <input className="form-control" type="number" name="" id="b_guest" placeholder="2" />
+                        <input className="form-control" type="number" name="" id="b_guest" value={guest} placeholder="2" onChange={(e) => setGuest(e.target.value)} />
                     </div>
 
                     <div className="input-box mb-3 d-flex justify-content-center">
-                        <input className="btn btn-warning" type="submit" value="Book your accomodation now " />
+                        <input className="btn btn-warning" type="button" value="Book your accomodation now " onClick={handleSubmit} />
                     </div>
                 </form>
             </div>
--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 export default function Contact() {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [msg, setMsg] = useState('')
+
+    const handleSubmit = () => {
+        if (name.length === 0) {
+            alert("Name has left blank");
+        }
+        else if (email.length === 0) {
+            alert("Email id has left blank");
+        }
+        else if (msg.length === 0) {
+            alert("Message has left blank");
+        } else {
+            const url = 'http://localhost/webweaver/contact.php';
+            let fData = new FormData();
+            fData.append('name', name);
+            fData.append('email', email);
+            fData.append('msg', msg);
+
+            axios.post(url, fData)
+                .then(response => alert(response.data))
+                .catch(error => alert(error))
+        }
+    }
+
     return (
         <section className=" text-gray-100 px-8 py-12" style={{ fontFamily: "Inter" }} id="contact">
             <div className="text-center w-full">
@@ -21,18 +49,18 @@ export default function Contact() {
                 <div className="col-sm-12 col-lg-6 mt-5" style={{ textAlign: "left" }}>
                     <div>
                         <span className="uppercase text-sm text-gray-600 font-bold">Full Name</span>
-                        <input className="w-full bg-gray-200 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" placeholder="" />
+                        <input className="w-full bg-gray-200 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" value={name} placeholder="" onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className="mt-8">
                         <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
-                        <input className="w-full bg-gray-200 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" />
+                        <input className="w-full bg-gray-200 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className="mt-8">
                         <span className="uppercase text-sm text-gray-600 font-bold">Message</span>
-                        <textarea className="w-full h-32 bg-gray-200 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+                        <textarea className="w-full h-32 bg-gray-200 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" value={msg} onChange={(e) => setMsg(e.target.value)}></textarea>
                     </div>
                     <div className="mt-8">
-                        <button className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+                        <button className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline" onClick={handleSubmit}>
                             Send Message
                         </button>
                     </div>
